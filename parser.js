13b63ee670697b015b54d213489d83ffe65bac2b
Andrei Pirvulet <overdeathrap@gmail.com>
2014-04-06 05:12:29 -0400

    Added methods to get data from selector and to check if the page is single

--- parser.js
+++ parser.js
@@ -1,5 +1,9 @@
 var amqp = require('amqp');
 var amqpConfig = require('./config/amqp.js');
+var request = require('request');
+var cheerio = require('cheerio');
+
+//var messageData = require('./doc/amazon.json');
 
 var connection = amqp.createConnection(amqpConfig.connectionConfig);
 connection.on('ready', function () {
@@ -20,5 +24,51 @@ connection.on('ready', function () {
 
 function processParseJob(message){
 	var messageString = message.data.toString();
-	var messageData = JSON.parse(messageString); 
+	var messageData = JSON.parse(messageString);
+
+	if(messageData.target == undefined || messageData.target.url == undefined) {
+		return false;
+	}
+
+	request(messageData.target.url, function (err, response, body) {
+	    if (!err && response.statusCode == 200) {
+		    console.log(body);
+		    var $ = cheerio.load(body);
+		    
+		    console.log($(messageData.rules.singleDetector.selector.name).text());
+		    console.log(isSinglePage(messageData.rules.singleDetector, $));
+	  	}
+	});
+}
+
+function isSinglePage(detector, cheerio) {
+	selectorData = getSelectorData(detector.selector, cheerio);
+	if (detector.compareExact != undefined) {
+		return getSelectorData == detector.compareExact;
+	}
+}
+
+function getSelectorData(selector, cheerio) {
+	if (selector.name == undefined || selector.name == "") {
+		throw new Error('Invalid selector name');
+	}
+	var selectorName = selector.name;
+	if (selector.method == undefined && selector.attr == undefined) {
+		selector.method = "text";
+	}
+
+	if (selector.method != undefined) {
+		switch(selector.method) {
+			case "length":
+				return cheerio(selectorName).length;
+			case "html":
+				return cheerio(selectorName).html();
+			case "text":
+				return cheerio(selectorName).text();
+			default:
+				throw new Error('Invalid selector method'); 
+		}
+	} else if(selector.attr != undefined) {
+		return cheerio(selectorName).attr(selector.attr);
+	}
 }
